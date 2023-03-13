eb616adc407f66db1a4bb61d456cb0dde395ddf3
Your Name <you@example.comclear>
2023-03-13 16:03:14 +0300

    completed assignments

---
+++ index.js
@@ -0,0 +1,45 @@
+function add() {
+
+}
+function add() {
+  // we'll fill this in shortly
+}
+function subtract() {
+  // we'll fill this in shortly
+}
+function multiply() {
+  // we'll fill this in shortly
+}
+function divide() {
+  // we'll fill this in shortly
+}
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function increment(n){
+  return n + 1;
+}
+function decrement(n){
+  return n - 1;
+}
+function makeInt(string){
+  return string;
+}
+function preserveDecimal(string){
+  return string;
+}
+function makeInt(n){
+ return parseInt(n ,10);
+}
+function preserveDecimal(n){
+  return parseFloat(n, 10);
+ }
